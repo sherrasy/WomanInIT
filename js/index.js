3f492d03ac2cb49d3243d30f11f56dcc9c375c2f
Anastasia <dzheyn.anna@mail.ru>
2024-03-09 16:52:29 +0400

    add some js

--- js/index.js
+++ js/index.js
@@ -2,17 +2,26 @@ const resumeForm = document.querySelector('.resume_form');
 const resumeButton = resumeForm.querySelector('button[type=submit]');
 const notesForm = document.querySelector('.notes_form');
 const addNotesButton = notesForm.querySelector('button[type=submit]');
-const deleteNotesButton = resumeForm.querySelector('');
+const deleteNotesButton = document.querySelector('#delete-notes');
+const userName = document.querySelector('#username');
+const notesList = document.querySelector('.notes_block__list');
+const getName = () => {
+  let name = prompt('Как к вам обращаться?');
+  if (name == null || name == '') {
+    name = 'Неопознанная леди';
+  }
+  localStorage.setItem('username', name);
+};
 
 const setResumeFormSubmit = () => {
   const resumeData = new FormData(resumeForm);
   const resumeInfo = {
-    features:resumeData.getAll('features'),
-    avatar : resumeData.get('avatar'),
-    description : resumeData.get('description'),
-  }
-  console.log(resumeInfo)
-  localStorage.setItem('resumeInfo', JSON.stringify(resumeInfo));    
+    features: resumeData.getAll('features'),
+    avatar: resumeData.get('avatar'),
+    description: resumeData.get('description'),
+  };
+  console.log(resumeInfo);
+  localStorage.setItem('resumeInfo', JSON.stringify(resumeInfo));
 };
 
 resumeButton.addEventListener('click', (evt) => {
@@ -20,20 +29,22 @@ resumeButton.addEventListener('click', (evt) => {
   setResumeFormSubmit();
 });
 
-
 const setNotesFormSubmit = () => {
-    const resumeData = new FormData(resumeForm);
-    const resumeInfo = {
-      features:resumeData.getAll('features'),
-      avatar : resumeData.get('avatar'),
-      description : resumeData.get('description'),
-    }
-    console.log(resumeInfo)
-    localStorage.setItem('resumeInfo', JSON.stringify(resumeInfo));    
-  };
-  
+  const newLi = document.createElement('li');
+  const text = document.createElement('span');
+  const notesData = new FormData(notesForm);
+  const note = notesData.get('note');
+  text.append(note);
+  notesList.appendChild(newLi).append(text);
+  const notes = []
+  notesList.querySelectorAll('li').forEach((item, i) => {
+    notes.push({ id: i, text: item.innerText });
+  });
+  localStorage.setItem('notes', JSON.stringify(notes));
+};
 
 addNotesButton.addEventListener('click', (evt) => {
   evt.preventDefault();
-  setResumeFormSubmit();
+  setNotesFormSubmit();
 });
+
